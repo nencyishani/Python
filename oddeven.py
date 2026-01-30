39c37706055d5417dd88598447d3579135ee1a86
nencyishani <nency.ishani127387@marwadiuniversity.ac.in>
2026-01-30 10:02:19 +0530

    Delete oddeven.py

--- oddeven.py
+++
@@ -1,9 +0,0 @@
-#write a program to print odd and even
-#roll no:92400527179,Name:Nency Ishani
-
-num=int(input("Enter a number:"))
-
-if num %2==0:
-    print(f"{num} is an Even number.")
-else:
-    print(f"{num} is an odd number.")
